aa46d6d150dff49acbc11f854d6a8f0012ea0670
Timothy Johnstone <timothy.g.johnstone@gmail.com>
2020-03-19 19:13:27 -0700

    no iam perm needed

--- cmd/sync.go
+++ cmd/sync.go
@@ -26,7 +26,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/spf13/cobra"
